4585e8ff1fb185777f127d80f15ad522c54a39f4
Matt <157317090+pan-kroczek@users.noreply.github.com>
2024-12-08 15:12:18 +0100

    Created getComputerChoice function
    A function generating a random choice (rock, paper or scissors) for computer's side

--- javascript.js
+++ javascript.js
@@ -1,1 +1,17 @@
-console.log('hello world');
+console.log('hello');
+
+// FUNCTION GENERATING RANDOM ITEM FOR THE COMPUTER SIDE
+
+function getComputerChoice() {
+    let randomNumber = Math.floor(Math.random() * 3);
+
+    if (randomNumber === 0) {
+        return 'rock';
+    } else if (randomNumber === 1) {
+        return 'paper';
+    } else {
+        return 'scissors';
+    }
+}
+
+console.log(getComputerChoice());
